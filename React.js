39d5dc45fc685918337c0d8bbeb7ae44ebd1d1b6
VISHAL MARAPATLA <145882447+vishalmarapatla@users.noreply.github.com>
2025-09-18 00:22:54 +0530

    Create React.js

---
+++ React.js
@@ -0,0 +1,18 @@
+client/
+  ├── public/
+  ├── src/
+  │   ├── components/
+  │   │   ├── Auth/
+  │   │   ├── Blog/
+  │   │   ├── Comments/
+  │   │   ├── Dashboard/
+  │   │   ├── Layout/
+  │   │   ├── Profile/
+  │   │   └── UI/
+  │   ├── pages/
+  │   ├── redux/
+  │   ├── utils/
+  │   ├── App.js
+  │   └── index.js
+  ├── package.json
+  └── tailwind.config.js
